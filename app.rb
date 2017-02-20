e5e149b9fc7b78faeed5aca224599bcecc55e60e
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-20 13:55:55 -0500

    app

--- app.rb
+++ app.rb
@@ -4,7 +4,16 @@ class PersonalDetailsApp < Sinatra::Base
 
 	get '/' do
 		erb :name
-		
+	end
+
+	post '/name' do
+		name = params[:name_input]
+		redirect '/age?name=' + name
+	end
+
+	get '/age' do
+		name = params[:name]
+		erb :age, locals: {name: name}
 	end
 
 end
--- tests/test_app.rb
+++ tests/test_app.rb
@@ -15,4 +15,18 @@ class TestApp < Minitest::Test
 		assert(last_response.body.include?('Hello, what is your name?'))
 		assert(last_response.body.include?('<input type="text" name="name_input">'))
 	end
+
+	def test_post_to_name
+		post '/name', name_input: 'Chloe'
+		follow_redirect!
+		assert(last_response.body.include?('Chloe'))
+		assert(last_response.ok?)
+	end
+
+	def test_get_age
+		get '/age?name=Chloe'
+		assert(last_response.body.include?('Hello Chloe! How old are you?'))
+		assert(last_response.ok?)
+	end
+
 end
